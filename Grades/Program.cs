af8a51f08762196aea88f78bb63f92657c80812b
gorohoroh <gorohoroh@gmail.com>
2018-07-31 18:08:17 +0300

    Program.cs: extracting to methods

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -9,6 +9,43 @@ namespace Grades
         {
             GradeBook book = new GradeBook();
             book.NameChanged += OnNameChanged;
+            
+            PromptForGradebookName(book);
+            EnterGrades(book);
+            SaveGradesToFile(book);
+
+            var stats = book.ComputeStatistics();
+
+            WriteStatsToConsole(book, stats);
+        }
+
+        private static void WriteStatsToConsole(GradeBook book, GradeStatistics stats)
+        {
+            Console.WriteLine(book.Name);
+            Console.WriteLine("Average grade: " + stats.AverageGrade);
+            Console.WriteLine("{0}: {1:F1}", "Lowest grade", stats.LowestGrade);
+            Console.WriteLine($"Highest grade: {stats.HighestGrade:F1}");
+            Console.WriteLine($"Letter grade: {stats.LetterGrade}");
+            Console.WriteLine($"Grade description: {stats.GradeDescription}");
+        }
+
+        private static void SaveGradesToFile(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt")) {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void EnterGrades(GradeBook book)
+        {
+            book.AddGrade(75);
+            book.AddGrade(99);
+            book.AddGrade(79.5f);
+            book.AddGrade(90.1f);
+        }
+
+        private static void PromptForGradebookName(GradeBook book)
+        {
             Console.WriteLine("Enter a gradebook name:");
 
             try {
@@ -20,25 +57,6 @@ namespace Grades
             catch (Exception) {
                 Console.WriteLine("Something went wrong. Did it?");
             }
-            
-            book.AddGrade(75);
-            book.AddGrade(99);
-            book.AddGrade(79.5f);
-            book.AddGrade(90.1f);
-
-            using (StreamWriter outputFile = File.CreateText("grades.txt")) {
-                book.WriteGrades(outputFile);
-            }
-
-            var stats = book.ComputeStatistics();
-
-            Console.WriteLine(book.Name);
-            Console.WriteLine("Average grade: " + stats.AverageGrade);
-            Console.WriteLine("{0}: {1:F1}", "Lowest grade", stats.LowestGrade);
-            Console.WriteLine($"Highest grade: {stats.HighestGrade:F1}");
-            Console.WriteLine($"Letter grade: {stats.LetterGrade}");
-            Console.WriteLine($"Grade description: {stats.GradeDescription}");
-
         }
 
         static void OnNameChanged(object sender, NameChangedEventArgs args)
