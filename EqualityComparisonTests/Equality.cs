8a2d0af9af068bc0bb5a46fd3b70f8a18ddc0624
gorohoroh <gorohoroh@gmail.com>
2018-08-09 16:22:08 +0300

    The static Equals() method and handling null references

--- EqualityComparisonTests/Equality.cs
+++ EqualityComparisonTests/Equality.cs
@@ -62,5 +62,29 @@ namespace EqualityComparisonTests
             
             Assert.True(foodStruct.Equals(foodStruct2));
         }
+
+        [Fact]
+        public void ObjectEquals_Null_AlwaysNotEqualToNonNull_ReturnsFalse()
+        {
+            Food food = new Food("banana");
+            
+            Assert.False(food.Equals(null));
+        }
+
+        [Fact]
+        public void ObjectEqualsStatic_AvoidsNREs_ReturnsFalse()
+        {
+            Food food = new Food("banana");
+            
+            // The instance method object.Equals() would cause a NRE when invoked on a null reference; to avoid this, the static Object.Equals() is used that takes two parameters,
+            // either of which can be null. The static method provides null checking, and if neither parameter is null, it calls the instance method.
+            Assert.False(Equals(null, food));
+        }
+
+        [Fact]
+        public void ObjectEqualsStatic_TwoNullsAreAlwaysEqual_ReturnsTrue()
+        {
+            Assert.True(Equals(null, null));
+        }
     }
 }
