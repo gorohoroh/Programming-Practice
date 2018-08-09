4cd085d8551e492c5596774ddedd99f1fe7ccc27
gorohoroh <gorohoroh@gmail.com>
2018-08-10 00:13:12 +0300

    Implementing equality for a value type: overloaded equality and inequality operators + grouped the Equals(object obj) override with other equality members

--- FoodEquals/FoodItem.cs
+++ FoodEquals/FoodItem.cs
@@ -4,15 +4,6 @@ namespace FoodEquals
 {
     public struct FoodItem : IEquatable<FoodItem>
     {
-        public override bool Equals(object obj)
-        {
-            if (obj is FoodItem) {
-                return Equals((FoodItem)obj);
-            }
-
-            return false;
-        }
-
         private readonly string name;
         private readonly FoodGroup group;
 
@@ -32,9 +23,29 @@ namespace FoodEquals
             this.group = group;
         }
 
+        public static bool operator ==(FoodItem lhs, FoodItem rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(FoodItem lhs, FoodItem rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+
         public bool Equals(FoodItem other)
         {
             return name == other.name && group == other.group;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is FoodItem) {
+                return Equals((FoodItem)obj);
+            }
+
+            return false;
         }
 
         public override string ToString()
