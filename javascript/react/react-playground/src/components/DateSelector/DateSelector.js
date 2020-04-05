24e597952298453cbc4a4a57cf63fe2be34ebd0d
gorohoroh <gorohoroh@gmail.com>
2020-04-05 16:53:40 +0300

    Date selector displays available decades and highlights the active decade. Report filters albums based on a decade it receives from the App, passes the resulting set to data list.

--- javascript/react/react-playground/src/components/DateSelector/DateSelector.js
+++ javascript/react/react-playground/src/components/DateSelector/DateSelector.js
@@ -2,7 +2,31 @@ import * as React from "react";
 
 export default class DateSelector extends React.Component {
     render() {
-        return <p>Wassup, I'm a date selector</p>;
+
+        const decades = [
+            { startYear: "1940" },
+            { startYear: "1950" },
+            { startYear: "1960" },
+            { startYear: "1970" },
+            { startYear: "1980" },
+            { startYear: "1990" },
+            { startYear: "2000" },
+            { startYear: "2010" },
+            { startYear: "2020" }
+        ].map(decade => {
+            if (decade.startYear === this.props.decade) decade.active = true;
+            return decade;
+        });
+
+        return <div className="date-selector">
+            <p>Wassup, I'm a decade selector. I display a set of decades available for selection, and highlight the selected decade. When someone selects a new decade, I pass that to my parent, <code>Report</code>, to refresh data and re-render myself and <code>DataList</code>.</p>
+            <ul className="date-selector-list">
+                {decades.map(item => item.active ?
+                    <li key={item.startYear} className="decade-active">{item.startYear}s</li> :
+                    <li key={item.startYear}>{item.startYear}s</li>)
+                }
+            </ul>
+        </div>;
     }
 
 }
--- javascript/react/react-playground/src/components/Report/Report.js
+++ javascript/react/react-playground/src/components/Report/Report.js
@@ -8,27 +8,29 @@ export default class Report extends React.Component {
     constructor(props) {
         super(props);
 
-        // const topAlbumsByDecade = topAlbums.filter(item => item.year)
+        const topAlbumsInDecade = topAlbums.filter(item => {
+            const startYear = Number.parseInt(this.props.decade);
+            const endYear = startYear+10;
+            if(item.year >= startYear && item.year < endYear) return item;
+        });
 
         this.state = {
-            albums: topAlbums
+            albums: topAlbumsInDecade
         };
-
-        console.log(this.state.albums)
     }
 
     render() {
-        return !this.state.albums ? "Waiting for data" :
+        return !this.state.albums ? <p>"Waiting for data"</p> :
         <div className="report">
-            <h1>Wassup, I'm your whole report</h1>
+            <h1>Wassup, I'm your Top 500 albums report</h1>
             <p>I get some initial data from my containing application &mdash; that is, a decade that should be preselected:</p>
             <ul>
                 <li><code>{this.props.decade}</code></li>
             </ul>
-            <p>I pass the preselected decade to <code>DecadeSelector</code>. <code>DecadeSelector</code> can then be used to select another decade. When it does, it should pass the selected decade up to <code>Report</code>. <code>Report</code> should made a new album selection mathing the new decade, and pass the results to both <code>DecadeSelector</code> (new selected decade) and <code>DataList</code> (a new set of albums)</p>
-            <p>I also store my own data: a selection of albums matching a selected decade. It pass it to <code>DateSelector</code> that I render with <code>DataList</code>.</p>
+            <p>I pass the preselected decade to <code>DecadeSelector</code>. <code>DecadeSelector</code> can then be used to select another decade. When it does, it should pass the selected decade up to <code>Report</code>. <code>Report</code> should made a new album selection matching the new decade, and pass the results to both <code>DecadeSelector</code> (new selected decade) and <code>DataList</code> (a new set of albums)</p>
+            <p>I also store my own data: a selection of albums matching a selected decade. I render the albums using <code>DataList</code>.</p>
 
-            <DateSelector/>
+            <DateSelector decade={this.props.decade}/>
 
             <DataList albums={this.state.albums}/>
         </div>
