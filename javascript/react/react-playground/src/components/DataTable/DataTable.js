dafb0f835a9a085a3ab79b4bfd8d363380710771
gorohoroh <gorohoroh@gmail.com>
2020-04-06 12:12:45 +0300

    Data Table: fixing table structure

--- javascript/react/react-playground/src/components/DataTable/DataTable.js
+++ javascript/react/react-playground/src/components/DataTable/DataTable.js
@@ -5,22 +5,24 @@ export default class DataTable extends React.Component {
     render() {
         return !this.props.albums.length ? "No albums in this decade" :
             <table>
-                <tr>
-                    <th>Ranking</th>
-                    <th>Artist</th>
-                    <th>Name</th>
-                    <th>Year</th>
-                </tr>
-                {
+                <thead>
+                    <tr>
+                        <th>Ranking</th>
+                        <th>Artist</th>
+                        <th>Name</th>
+                        <th>Year</th>
+                    </tr>
+                </thead>
+                <tbody>{
                     this.props.albums.map(album => {
-                        return <tr>
+                        return <tr key={album.number}>
                             <td>{album.number}</td>
                             <td>{album.artist}</td>
                             <td>{album.name}</td>
                             <td>{album.year}</td>
                         </tr>
                     })
-                }
+                }</tbody>
             </table>
     }
 }
