3163dfe158b1f75ced9b1cb37687c995e9b25816
gorohoroh <gorohoroh@gmail.com>
2021-05-09 00:35:24 +0300

    Extracting a component to represent a single todo item

--- javascript/vue/todo-app/src/components/TodoList.vue
+++ javascript/vue/todo-app/src/components/TodoList.vue
@@ -3,40 +3,18 @@
     <p>Completed tasks: {{ todos.filter(todo => todo.done).length }}</p>
     <p>Pending tasks: {{ todos.filter(todo => !todo.done).length }}</p>
 
-
-    <div class="ui centered card" v-for="todo in todos">
-      <div class="content">
-        <div class="header">
-          {{ todo.title }}
-        </div>
-        <div class="meta">
-          {{ todo.project }}
-        </div>
-        <div class='extra content'>
-            <span class='right floated edit icon'>
-              <i class='edit icon'></i>
-            </span>
-        </div>
-      </div>
-
-      <div class='ui bottom attached green basic button' v-show="todo.done">
-        Completed
-      </div>
-      <div class='ui bottom attached red basic button' v-show="!todo.done">
-        Complete
-      </div>
-    </div>
-
+    <todo v-for="todo in todos" v-bind:todo="todo"></todo>
 
   </div>
 </template>
 
 <script type="text/javascript">
 
+import Todo from "./Todo";
+
 export default {
-  props: ['todos']
+  props: ['todos'],
+  components: {Todo}
 };
 </script>
 
-<style>
-</style>
