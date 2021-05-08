f44b7541d2f6e60d17711668279101f55801b00d
gorohoroh <gorohoroh@gmail.com>
2021-05-08 23:58:58 +0300

    Adding a TodoList component, HelloWorld goes away

--- javascript/vue/todo-app/src/App.vue
+++ javascript/vue/todo-app/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoList from "./components/TodoList";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
--- javascript/vue/todo-app/test/unit/specs/HelloWorld.spec.js
+++ javascript/vue/todo-app/test/unit/specs/HelloWorld.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import TodoList from '@/components/TodoList'
 
-describe('HelloWorld.vue', () => {
+describe('TodoList.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(TodoList)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toEqual('Welcome to Your Vue.js App')
