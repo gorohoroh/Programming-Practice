919edd6e3bd886156c4c183ce1701574e19a4a80
gorohoroh <gorohoroh@gmail.com>
2021-05-09 00:12:18 +0300

    Sample todo data and making the TodoList component's template use it

--- javascript/vue/todo-app/src/App.vue
+++ javascript/vue/todo-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <todo-list></todo-list>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -11,8 +11,30 @@ export default {
   name: 'App',
   components: {
     TodoList
-  }
-}
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
+
 </script>
 
 <style>
