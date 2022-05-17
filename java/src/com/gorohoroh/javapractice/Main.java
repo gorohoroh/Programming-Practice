1ba022692497dbf7e93699a7333fddfee4ce77e8
Jura Gorohovsky <gorohoroh@gmail.com>
2022-05-17 13:43:33 +0300

    String formatting

--- java/src/com/gorohoroh/javapractice/Main.java
+++ java/src/com/gorohoroh/javapractice/Main.java
@@ -1,11 +1,35 @@
 package com.gorohoroh.javapractice;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        primitiveNumericTypeLimitations();
+//        primitiveNumericTypeLimitations();
+        stringFormatting();
+    }
+
+    private static void stringFormatting() {
+
+        List<Player> playersOverThousandWins = Arrays.asList(
+                new Player("Roger Federer", 40, 2015),
+                new Player("Novak Djokovic", 34, 2022),
+                new Player("Rafael Nadal", 35, 2020));
+        Player federer = playersOverThousandWins.get(0);
+
+        String stringFormat = String.format("There are %d active tennis players who have won at least 1000 matches in their careers. One of them is %s (age %d) who has won their 1000th match in %d.", playersOverThousandWins.size(), federer.name, federer.age, federer.yearThousandMatchesWon);
+
+        String messageFormat = MessageFormat.format("There are {0} active tennis players who have won at least 1000 matches in their careers. One of them is {1} (age {2}) who has won their 1000th match in {3}.", playersOverThousandWins.size(), federer.name, federer.age, String.format("%d", federer.yearThousandMatchesWon));
+
+        String averageAgeWithStringFormatAndStream = String.format("The average age of the three players is %.1f years", playersOverThousandWins.stream().mapToDouble(value -> value.age).average().orElse(Double.NaN));
+
+        String currentTimeInMilliseconds = String.format("%,d", System.currentTimeMillis());
+
+        System.out.println(stringFormat);
+        System.out.println(averageAgeWithStringFormatAndStream);
+        System.out.println(currentTimeInMilliseconds);
     }
 
     private static void primitiveNumericTypeLimitations() {
---
+++ java/src/com/gorohoroh/javapractice/Player.java
@@ -0,0 +1,13 @@
+package com.gorohoroh.javapractice;
+
+public class Player {
+    public String name;
+    public int age;
+    public int yearThousandMatchesWon;
+
+    public Player(String name, int age, int yearThousandMatchesWon) {
+        this.name = name;
+        this.age = age;
+        this.yearThousandMatchesWon = yearThousandMatchesWon;
+    }
+}
